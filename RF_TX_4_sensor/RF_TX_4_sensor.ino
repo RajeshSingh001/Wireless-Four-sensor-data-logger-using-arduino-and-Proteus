43c85f7babdc329a8eb2f240a70ba9932155c37f
Dr. Rajesh Singh <75157838+RajeshSingh001@users.noreply.github.com>
2020-11-30 10:15:54 +0530

    TX and RX code, Proteus Model

---
+++ RF_TX_4_sensor/RF_TX_4_sensor.ino
@@ -0,0 +1,47 @@
+
+#include<LiquidCrystal.h>;
+//int buttonState = 0;// variable for reading the pushbutton status
+LiquidCrystal lcd(12,11,5,4,3,2);
+
+void setup() {
+  // initialize the LED pin as an output:   
+  Serial.begin(9600);
+  lcd.begin(20,4);
+  lcd.setCursor(0,0);
+  lcd.print("Welcome to Data");
+  lcd.setCursor(0,1);
+  lcd.print("Transmission");
+  delay(2000);
+  lcd.clear(); 
+}
+
+void loop(){
+  int sensorValue1 = analogRead(A0);
+  int sensorValue2 = analogRead(A1);
+  int sensorValue3 = analogRead(A2);
+  int sensorValue4 = analogRead(A3);
+  int TEMP1=sensorValue1/2;
+  int TEMP2=sensorValue2/2;
+   int TEMP3=sensorValue3/2;
+    int TEMP4=sensorValue4/2;
+  lcd.setCursor(0,0);
+  lcd.print("TEMP1:");
+  lcd.print(TEMP1);
+  lcd.setCursor(0,1);
+  lcd.print("TEMP2:");
+  lcd.print(TEMP2);
+  lcd.setCursor(0,2);
+  lcd.print("TEMP3:");
+  lcd.print(TEMP3);
+  lcd.setCursor(0,3);
+  lcd.print("TEMP4:");
+  lcd.print(TEMP4);
+  
+  Serial.print(TEMP1);
+  Serial.print(TEMP2);
+  Serial.print(TEMP3);
+  Serial.println(TEMP4);
+  
+  delay(200);
+  
+ }
---
+++ SerialEvent_string_4_sensor_RX/SerialEvent_string_4_sensor_RX.ino
@@ -0,0 +1,97 @@
+/*
+  Serial Event example
+ 
+ When new serial data arrives, this sketch adds it to a String.
+ When a newline is received, the loop prints the string and 
+ clears it.
+ 
+ A good test for this is to try it with a GPS receiver 
+ that sends out NMEA 0183 sentences. 
+ 
+ Created 9 May 2011
+ by Tom Igoe
+ 
+ This example code is in the public domain.
+ 
+ http://www.arduino.cc/en/Tutorial/SerialEvent
+ 
+ */
+ #include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+
+void setup() 
+{
+  lcd.begin(20,4);
+  // initialize serial:
+  Serial.begin(9600);
+  // reserve 200 bytes for the inputString:
+  inputString.reserve(200);
+}
+
+void loop()
+{
+  // print the string when a newline arrives:
+  if (stringComplete)
+  {
+    lcd.setCursor(0,0);
+    lcd.print(inputString[0]);
+    lcd.print(inputString[1]);
+    lcd.setCursor(0,1);
+    lcd.print(inputString[2]);
+    lcd.print(inputString[3]);
+    lcd.setCursor(0,2);
+    lcd.print(inputString[4]);
+    lcd.print(inputString[5]);
+    lcd.setCursor(0,3);
+    lcd.print(inputString[6]);
+    lcd.print(inputString[7]);
+    
+    if(inputString[0]==0x0A)
+    {
+     lcd.setCursor(0,0);
+    lcd.print(inputString[1]);
+    lcd.print(inputString[2]);
+    lcd.setCursor(0,1);
+    lcd.print(inputString[3]);
+    lcd.print(inputString[4]);
+    lcd.setCursor(0,2);
+    lcd.print(inputString[5]);
+    lcd.print(inputString[6]);
+    lcd.setCursor(0,3);
+    lcd.print(inputString[7]);
+    lcd.print(inputString[8]); 
+    }
+    Serial.println(inputString); 
+    // clear the string:
+    inputString = "";
+    stringComplete = false;
+  }
+}
+
+/*
+  SerialEvent occurs whenever a new data comes in the
+ hardware serial RX.  This routine is run between each
+ time loop() runs, so using delay inside loop can delay
+ response.  Multiple bytes of data may be available.
+ */
+void serialEvent()
+{
+  while (Serial.available()>0)
+  {
+    // get the new byte:
+    char inChar = (char)Serial.read(); 
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == 0x0D) 
+    {
+      stringComplete = true;
+    } 
+  }
+}
+
+
